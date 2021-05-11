c9d38d791b431d909ad091b73765be9be914f93a
nqueudot <nqueudot28@gmail.com>
2021-05-11 22:10:55 +0200

    page meteo fini manque titre footer et nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Journal from './component/journal';
+import Meteo from './component/Meteo';
 
 import logo from './logo.png';
 import './Mars.css';
@@ -10,14 +11,15 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Journal semaine={['lundi', '2021/05/03']} />
+      {/* <Journal semaine={['lundi', '2021/05/03']} />
       <Journal semaine={['mardi', '2021/05/04']} />
       <Journal semaine={['mercredi', '2021/05/05']} />
       <Journal semaine={['jeudi', '2021/05/06']} />
       <Journal semaine={['vendredi', '2021/05/07']} />
       <Journal semaine={['samedi', '2021/05/08']} />
       <Journal semaine={['dimanche', '2021/05/09']} />
-      <Robot />
+      <Robot /> 
+  <Meteo />*/}
     </div>
   );
 }
