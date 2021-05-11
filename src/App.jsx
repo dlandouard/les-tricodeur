83c6101d45fdef579dc61b6448fa710b880082bd
axel <chapellieraxel@gmail.com>
2021-05-11 16:07:37 +0200

    conflit resolue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import React from 'react';
-<<<<<<< HEAD
-import Journal from './component/journal'
+import Journal from './component/journal';
 
 import logo from './logo.png';
 import './Mars.css';
-=======
-import Robot from './component/Robot'
+import Robot from './component/Robot';
 
 import './App.css';
->>>>>>> dev
 
 function App() {
-
   return (
-<<<<<<< HEAD
-   <div>
-     <Journal semaine = {["lundi", "2021/05/03"]} />
-     <Journal semaine = {["mardi", "2021/05/04"]} />
-     <Journal semaine = {["mercredi", "2021/05/05"]} />
-     <Journal semaine = {["jeudi", "2021/05/06"]} />
-     <Journal semaine = {["vendredi", "2021/05/07"]} />
-     <Journal semaine = {["samedi", "2021/05/08"]} />
-     <Journal semaine = {["dimanche", "2021/05/09"]} />
-   </div>
-  );
-=======
-   <main>
+    <div>
+      <Journal semaine={['lundi', '2021/05/03']} />
+      <Journal semaine={['mardi', '2021/05/04']} />
+      <Journal semaine={['mercredi', '2021/05/05']} />
+      <Journal semaine={['jeudi', '2021/05/06']} />
+      <Journal semaine={['vendredi', '2021/05/07']} />
+      <Journal semaine={['samedi', '2021/05/08']} />
+      <Journal semaine={['dimanche', '2021/05/09']} />
       <Robot />
-   </main>
-  )
->>>>>>> dev
+    </div>
+  );
 }
 
 export default App;
