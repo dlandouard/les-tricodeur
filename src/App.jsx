9d43ba552c5cb57c3ee7a7fc65e1c1d9b52a4b62
nqueudot <nqueudot28@gmail.com>
2021-05-11 15:44:32 +0200

    Composent Robot terminé en version mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
+import Robot from './component/Robot'
 
-import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <main className="rsw-container">
-      <div
-        className="rsw-item"
-        style={{
-          paddingBottom: '2rem',
-        }}>
-        <img src={logo} width="20%" alt="WCS logo" />
-      </div>
-      <div className="rsw-item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="rsw-item">
-        <p>
-          Start in the <code>App.jsx</code> component !
-        </p>
-      </div>
-    </main>
-  );
+   <main>
+      <Robot />
+   </main>
+  )
 }
 
 export default App;
