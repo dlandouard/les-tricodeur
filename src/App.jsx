afbddbac04f42137361b5a8c50dd55cb20bbcbc9
axel <chapellieraxel@gmail.com>
2021-05-12 02:20:00 +0200

    test porte

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import './Uranus.css';
 import Culture from './component/Culture.jsx';
 import Journal from './component/journal';
-<<<<<<< HEAD
 import ALaUne from './component/ALaUne';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-=======
->>>>>>> dev
 import './Mars.css';
 import Robot from './component/Robot';
 
@@ -21,14 +18,14 @@ function App() {
         "Perseverance, nicknamed Percy, is a car-sized Mars rover designed to explore the Jezero crater on Mars as part of NASA's Mars 2020 mission.",
       robot: 'https://robohash.org/alien',
       avatar: 'Avatar de Perseverance',
-      end : false
+      end : false,
     },
     {
       image: './src/ingenuity.jpg',
       description: "Ingenuity is a small robotic solar helicopter operating on Mars as part of NASA's Mars 2020 mission.",
       robot: 'https://robohash.org/robot',
       avatar: 'Avatar de ingenuity',
-      end : true
+      end : true,
     },
   ];
 
@@ -37,9 +34,6 @@ function App() {
       <Switch>
         <Route exact path="/">
           <ALaUne />
-        </Route>
-        <Route path="/Journal">
-          <Journal />
         </Route>
         <Route path="/Robot">
           {robotInfo.map((text) => (
