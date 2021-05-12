73122231bb3a9bf464a690768d0229f337663b07
Damien Landouard <dlandouard@gmail.com>
2021-05-12 12:59:10 +0200

    logo statique et responsive

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="headerContainer">
-      <img id="mars" src="https://media.giphy.com/media/JRZwMhzk7WolG/giphy.gif" alt="Mars tourne" />
-      <img id="titre" src="./src/Titre.png" alt="Titre" />
+      <img id="titre" src="./src/Titre_statique.png" alt="Titre" />
     </div>
   );
 }
